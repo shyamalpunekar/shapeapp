bd2930a9dbb936bfb4461d4f33926310eb188088
shy <shyamal.punekar@gmail.com>
2017-08-02 08:07:15 -0700

    Add second test

---
+++ src/main/java/models/Rectangle.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created by Guest on 8/2/17.
+ */
+public class Rectangle {
+
+    public Rectangle(int height , int width){
+
+    }
+
+}
---
+++ src/test/java/models/RectangleTest.java
@@ -0,0 +1,23 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/2/17.
+ */
+public class RectangleTest {
+
+    @Test
+    public void newRectangle_instantiatesCorrectly() throws Exception{
+
+        Rectangle testRectangle = new Rectangle(2,4);
+        assertEquals(true, testRectangle instanceof Rectangle);
+
+
+
+    }
+
+
+}
